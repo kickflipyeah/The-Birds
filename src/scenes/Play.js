95d0f66368dfa68bb3ff1ed8eacfc88ee23f5f27
kickflipyeah <59582785+kickflipyeah@users.noreply.github.com>
2023-06-08 16:17:01 -0700

    fixing backgrounds

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -6,15 +6,17 @@ class Play extends Phaser.Scene {
         //add scrolling background
         let groundX=this.sys.game.config.width/2;
         let groundY=this.sys.game.config.height * .9;
-        this.sky = this.add.tileSprite(0, 0, game.config.width, 151, 'Sky').setOrigin(0, 0);
-        this.ground = this.physics.add.image(groundX, groundY, 'Ground');
-        this.ground.displayHeight=30;
-        this.ground.body.setAllowGravity(false);
-        this.ground.setImmovable();
-        this.bushes = this.add.tileSprite(0, 151, game.config.width, 89, 'Bushes').setOrigin(0, 0);
-
-
+        this.sky = this.add.tileSprite(0, 0, game.config.width, 187, 'Sky').setOrigin(0, 0);
+        this.groundbox = this.physics.add.image(groundX, groundY, 'Ground');
+        this.groundbox.displayHeight=50;
+        this.groundbox.body.setAllowGravity(false);
+        this.groundbox.setImmovable();
+        this.bushes = this.add.tileSprite(0, 209, game.config.width, 31, 'Bushes').setOrigin(0, 0); //209
+        this.ground = this.add.tileSprite(0, 187, game.config.width, 259, 'Ground').setOrigin(0, 0);
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0xfff5bb).setOrigin(0, 0);
     }
+    update(){
 
+        
+    }
 }
